576c4ec679001e3178c867c608613e5fe59e1293
pranavchiluveru <51951094+pranavchiluveru@users.noreply.github.com>
2023-06-12 12:34:00 +0530

    Create Palindrome String.cpp

---
+++ Palindrome String.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    int s1=S.size()-1;
+	    int i=0;
+	    while(i<s1)
+	    {
+	        if(S[i]==S[s1])
+	        {
+	        i++;
+	        s1--;
+	        }
+	        else
+	            return 0;
+	    }
+	    return 1;
+	    
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
